66f162275537093d0ea316911efb86e7d35cf83e
adezxc <adam.jasinski@unity3d.com>
2021-09-26 15:53:08 +0300

    more changes to the hashing function

--- main.go
+++ main.go
@@ -1,19 +1,47 @@
 package main
 
-import 
-(
-    "fmt"
-    "io/ioutil"
-    "log"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strconv"
+
+	//"math/bits"
+	"encoding/binary"
 )
+
 //import "encoding/hex"
 
 func main() {
-    src, err := ioutil.ReadFile("test.txt")
-    if err != nil {
-        log.Fatal(err)
+	src, err := ioutil.ReadFile("test.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(hash(src))
+}
+
+func hash(text []byte) string {
+    prime :=  1297
+	var hash uint64 = 0
+	if len(text) == 0 {
+		return ""
+	}
+	var char = binary.BigEndian.Uint64(text[0:])
+
+	for i := 0; i < len(text); i++ {
+		hash = ((hash << 5) & hash) + char
+	}
+
+	var charlist string = "0123456789abcdef"
+    var hashString = strconv.FormatUint(hash, 10)
+    fmt.Println(len(hashString))
+
+    result := ""
+    for i := 0; i < 64; i++ {
+        index := hashString[i % len(hashString)] + (byte)(i * prime);
+        result += (string)(charlist[index % (byte)(len(charlist))])
     }
-
-    fmt.Printf("%.8b", src)
-
+    
+	return result
 }
