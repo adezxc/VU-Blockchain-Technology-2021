9597cb837bc0eb9381c831406582a25161f2b4b6
adezxc <adam.jasinski@unity3d.com>
2021-10-25 12:40:00 -0300

    create blockchain package and remove deadcode

---
+++ blockchain/hash/prime.go
@@ -0,0 +1,47 @@
+package hash 
+
+func isPrime(n int) bool {
+    // Corner cases
+    if (n <= 1) {
+	    return false
+    }
+    if (n <= 3)  {
+	    return true
+    }
+    if (n%2 == 0 || n%3 == 0) {
+	    return false
+}
+   
+    for i := 5; i*i<=n; i+=6 {
+	if (n%i == 0 || n%(i+2) == 0) {
+		return false
+	}
+    }
+   
+    return true
+}
+ 
+// Function to return the smallest
+// prime number greater than N
+func nextPrime(n int) int {
+ 
+    // Base case
+    if (n <= 1) {
+	return 2
+    }
+
+    prime := n
+    found := false
+ 
+    // Loop continuously until isPrime returns
+    // true for a number greater than n
+    for !found {
+        prime++;
+ 
+        if (isPrime(prime)) {
+		found = true
+	}
+    }
+ 
+    return prime
+}
---
+++ blockchain/main.go
@@ -0,0 +1,9 @@
+package blockchain
+
+import (
+	"github.com/adezxc/VU-Blockchain-Technology-2021/hash"
+)
+
+func main() {
+	
+}
