8b1206c34825595b046e513939905a06d5f4c123
adezxc <adam.jasinski@unity3d.com>
2021-12-17 11:13:31 +0200

    yes

--- SmartContracts/src/components/Navbar.js
+++ SmartContracts/src/components/Navbar.js
@@ -4,18 +4,17 @@ class Navbar extends Component {
 
   render() {
     return (
-      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+      <nav className="navbar fixed-top flex-md-nowrap p-0 shadow">
         <a
           className="navbar-brand col-sm-3 col-md-2 mr-0"
           href="https://cardanofoundation.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Adam's Marketplace
         </a>
         <ul className="navbar-nav px-3">
           <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
-            <small className="text-white"><span id="account">{this.props.account}</span></small>
+            <small className="text-black"><span id="account">{this.props.account}</span></small>
           </li>
         </ul>
       </nav>
