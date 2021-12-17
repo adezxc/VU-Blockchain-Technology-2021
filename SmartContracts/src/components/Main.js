850ac940b08804e3dd10b5d5c732a4a8e8b2d9c3
adezxc <adam.jasinski@unity3d.com>
2021-12-17 02:39:04 +0200

    add smart contract

---
+++ SmartContracts/src/components/Main.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+
+  render() {
+    return (
+      <div id="content">
+        <h1>Pridėk produktą</h1>
+        <form onSubmit={(event) => {
+          event.preventDefault()
+          const name = this.productName.value
+          const price = window.web3.utils.toWei(this.productPrice.value.toString(), 'Ether')
+          this.props.createProduct(name, price)
+        }}>
+          <div className="form-group mr-sm-2">
+            <input
+              id="productName"
+              type="text"
+              ref={(input) => { this.productName = input }}
+              className="form-control"
+              placeholder="Produkto pavadinimas"
+              required />
+          </div>
+          <div className="form-group mr-sm-2">
+            <input
+              id="productPrice"
+              type="text"
+              ref={(input) => { this.productPrice = input }}
+              className="form-control"
+              placeholder="Produkto kaina"
+              required />
+          </div>
+          <button type="submit" className="btn btn-primary">Add Product</button>
+        </form>
+        <p>&nbsp;</p>
+        <h2>Produktų sąrašas</h2>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Pavadinimas</th>
+              <th scope="col">Kaina</th>
+              <th scope="col">Savininkas</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody id="productList">
+            { this.props.products.map((product, key) => {
+              return(
+                <tr key={key}>
+                  <th scope="row">{product.id.toString()}</th>
+                  <td>{product.name}</td>
+                  <td>{window.web3.utils.fromWei(product.price.toString(), 'Ether')} Eth</td>
+                  <td>{product.owner}</td>
+                  <td>
+                    { !product.purchased
+                      ? <button
+                          name={product.id}
+                          value={product.price}
+                          onClick={(event) => {
+                            this.props.purchaseProduct(event.target.name, event.target.value)
+                          }}
+                        >
+                          Buy
+                        </button>
+                      : null
+                    }
+                    </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Main;
